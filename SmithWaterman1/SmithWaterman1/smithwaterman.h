5c266a36cad715bd7ba71632e7ecf3da2c918be7
CPlusPlusSharp <64662103+CPlusPlusSharp@users.noreply.github.com>
2020-05-05 17:02:00 +0200

    Modified smithwaterman.h

--- SmithWaterman1/SmithWaterman1/smithwaterman.h
+++ SmithWaterman1/SmithWaterman1/smithwaterman.h
@@ -1,8 +1,20 @@
 #ifndef SMITHWATERMAN_H
 #define SMITHWATERMAN_H
 
+#include <string>
+#include <fstream>
+
 class smith_waterman
 {
+private:
+	std::string fileName1;
+	std::string fileName2;
+
+	std::string sequence1;
+	std::string sequence2;
+
+public:
+	smith_waterman(std::string, std::string);
 };
 
 #endif
